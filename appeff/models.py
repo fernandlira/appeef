e6dae9c209db3069a8c0064fe346d42b7fe77769
elmerkafu <kafupaucarpura5@gmail.com>
2020-07-11 16:42:31 -0500

    lista menu listo

--- appeff/models.py
+++ appeff/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -15,6 +16,10 @@ class Conductor(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_absolute_url(self):
+        return reverse("appeff:aceptar.viaje", kwargs={"id": self.id})
+    
 
 
 class Auto(models.Model):
